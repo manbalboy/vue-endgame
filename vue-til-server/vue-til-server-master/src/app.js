ebc3937361dc96298d707f4d012978f022f94cd3
manbalboy <manbalboy@hanmail.net>
2021-02-21 22:09:35 +0900

    몽고 DB 설정

--- vue-til-server/vue-til-server-master/src/app.js
+++ vue-til-server/vue-til-server-master/src/app.js
@@ -19,7 +19,7 @@ import { authenticateUser } from './utils/auth.js';
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 mongoose.connect(
-  'mongodb+srv://test:1234@cluster0-ypgh5.mongodb.net/test?retryWrites=true&w=majority',
+  'mongodb+srv://manbalboy:3wkdqjq4^^@cluster0.ss9n0.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
   },

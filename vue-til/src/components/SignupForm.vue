5159aa188910b536ae9577395522a2842205a187
manbalboy <manbalboy@hanmail.net>
2021-02-22 13:51:11 +0900

    회원가입 폼 이벤트 등록

--- vue-til/src/components/SignupForm.vue
+++ vue-til/src/components/SignupForm.vue
@@ -1,23 +1,36 @@
 <template>
-    <form>
+    <form @submit.prevent="submitForm">
         <div>
             <label for="username">ID : </label>
-            <input id="username" type="text" />
+            <input id="username" type="text" v-model="username" />
         </div>
         <div>
             <label for="password">PW : </label>
-            <input id="password" type="text" />
+            <input id="password" type="text" v-model="password" />
         </div>
         <div>
             <label for="nickname">NICKNAME : </label>
-            <input id="nickname" type="text" />
+            <input id="nickname" type="text" v-model="nickname" />
         </div>
         <button type="submit">LOGIN</button>
     </form>
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            username: '',
+            password: '',
+            nickname: '',
+        };
+    },
+    methods: {
+        submitForm() {
+            console.log('str >>>>> ', '폼제출');
+        },
+    },
+};
 </script>
 
 <style></style>

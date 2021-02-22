a4d1cb5977ff3fc46c28d97d7b2cefecdd36b693
manbalboy <manbalboy@hanmail.net>
2021-02-22 17:35:28 +0900

    로그인구현

---
+++ vue-til/src/components/LoginForm.vue
@@ -0,0 +1,29 @@
+<template>
+    <form @submit.prevent="submitForm">
+        <div>
+            <label for="username">ID : </label>
+            <input id="username" type="text" v-model="username" />
+        </div>
+        <div>
+            <label for="password">PW : </label>
+            <input id="password" type="text" v-model="password" />
+        </div>
+        <button type="submit">로그인</button>
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            password: '',
+            username: '',
+        };
+    },
+    methods: {
+        submitForm() {},
+    },
+};
+</script>
+
+<style></style>

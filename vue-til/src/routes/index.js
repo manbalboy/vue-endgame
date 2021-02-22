1f2424a4974f4ebdb19e09ef8a574f11fe8da2e6
manbalboy <manbalboy@hanmail.net>
2021-02-22 11:45:06 +0900

    라우팅 셋업

--- vue-til/src/routes/index.js
+++ vue-til/src/routes/index.js
@@ -1,5 +1,23 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import LoginPage from '@/views/LoginPage.vue';
+import SignupPage from '@/views/SignupPage.vue';
 
 Vue.use(VueRouter);
-export default new VueRouter({});
+export default new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            redirect: '/login',
+        },
+        {
+            path: '/login',
+            component: LoginPage,
+        },
+        {
+            path: '/signup',
+            component: SignupPage,
+        },
+    ],
+});
---
+++ vue-til/src/views/SignupPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        SignupPage
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>

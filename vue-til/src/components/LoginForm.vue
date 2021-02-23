3d3ae835926ca4f1f1b661edf06ccf4722e9f065
manbalboy <manbalboy@hanmail.net>
2021-02-23 17:12:10 +0900

    actions 추가 login 모듈화

--- vue-til/src/components/LoginForm.vue
+++ vue-til/src/components/LoginForm.vue
@@ -32,7 +32,6 @@
 </template>
 
 <script>
-import { loginUser } from '@/api/index';
 import { validateEmail } from '@/utils/validation';
 
 export default {
@@ -57,12 +56,7 @@ export default {
                     username: this.username,
                     password: this.password,
                 };
-
-                const { data } = await loginUser(userData);
-                console.log(data.token);
-
-                this.$store.commit('setToken', data.token);
-                this.$store.commit('setUsername', data.user.username);
+                this.$store.dispatch('LOGIN', userData);
                 this.$router.push(`${process.env.VUE_APP_BASE_URL}/main`);
             } catch (error) {
                 console.log(error.response.data);
--- vue-til/src/store/index.js
+++ vue-til/src/store/index.js
@@ -1,15 +1,39 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import {
+    getUserFromCookie,
+    getAuthFromCookie,
+    saveAuthToCookie,
+    saveUserToCookie,
+} from '@/utils/cookies';
+import { loginUser } from '@/api/index';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
-        username: '',
-        token: '',
+        username: getUserFromCookie() || '',
+        token: getAuthFromCookie() || '',
     },
     getters: {
         isLogin(state) {
             return state.username === '' ? false : true;
+        },
+    },
+    actions: {
+        async LOGIN(context, userData) {
+            try {
+                const { data } = await loginUser(userData);
+                console.log(data.token);
+
+                context.commit('setToken', data.token);
+                context.commit('setUsername', data.user.username);
+                saveAuthToCookie(data.token);
+                saveUserToCookie(data.user.username);
+                return data;
+            } catch (err) {
+                console.log(err);
+            }
         },
     },
     mutations: {

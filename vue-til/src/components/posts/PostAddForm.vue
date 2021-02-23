6b1114826aa4acf53b42baa51e9b195609ca9844
manbalboy <manbalboy@hanmail.net>
2021-02-23 17:45:56 +0900

    data binding 이벤트 연결 적용

--- vue-til/src/components/posts/PostAddForm.vue
+++ vue-til/src/components/posts/PostAddForm.vue
@@ -2,14 +2,19 @@
     <div class="contents">
         <h1 class="page-header">Create Post</h1>
         <div class="form-wrapper">
-            <form class="form" @submit.prevent="">
+            <form class="form" @submit.prevent="submitForm">
                 <div>
                     <label for="title">Title : </label>
-                    <input id="title" type="text" />
+                    <input id="title" type="text" v-model="title" />
                 </div>
                 <div>
                     <label for="contents">Contents : </label>
-                    <textarea id="contents" type="text" rows="5" />
+                    <textarea
+                        id="contents"
+                        type="text"
+                        rows="5"
+                        v-model="contents"
+                    />
                 </div>
                 <button type="submit" class="btn">Create</button>
             </form>
@@ -18,7 +23,19 @@
 </template>
 
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            title: '',
+            contents: '',
+        };
+    },
+    methods: {
+        submitForm() {
+            console.log('1');
+        },
+    },
+};
 </script>
 
 <style scoped>

c9b5463b0c5e53bdd3a51508b60e07ed3f77f236
manbalboy <manbalboy@hanmail.net>
2021-02-23 15:46:19 +0900

    PostListItem 컴포넌트화 적용

--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -3,29 +3,27 @@
         <div class="main list-container contents">
             <h1 class="page-header">Today I Learned</h1>
             <ul>
-                <li v-for="postItem in postItems" v-bind:key="postItem._id">
-                    <div class="post-title">
-                        {{ postItem.title }}
-                    </div>
-                    <div class="post-contents">
-                        {{ postItem.contents }}
-                    </div>
-                    <div class="post-time">
-                        {{ postItem.createdAt }}
-                    </div>
-                </li>
+                <PostListItem
+                    v-for="postItem in postItems"
+                    v-bind:key="postItem._id"
+                    :postItem="postItem"
+                ></PostListItem>
             </ul>
         </div>
     </div>
 </template>
 
 <script>
+import PostListItem from '@/components/posts/PostListItem.vue';
 import { fetchPosts } from '@/api/index';
 export default {
     data() {
         return {
             postItems: [],
         };
+    },
+    components: {
+        PostListItem,
     },
     methods: {
         async fetchData() {

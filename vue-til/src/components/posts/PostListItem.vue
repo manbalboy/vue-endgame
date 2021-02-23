6e285f7f59b816c8ad0118589e1a8c81f1dd8c7c
manbalboy <manbalboy@hanmail.net>
2021-02-23 22:29:18 +0900

    filter

--- vue-til/src/components/posts/PostListItem.vue
+++ vue-til/src/components/posts/PostListItem.vue
@@ -7,7 +7,7 @@
             {{ postItem.contents }}
         </div>
         <div class="post-time">
-            {{ postItem.createdAt | formatData }}
+            {{ postItem.createdAt | formatDate }}
             <i class="icon ion-md-create" @click="routeEditPage"></i>
             <i class="icon ion-md-trash" @click="deleteItem"></i>
         </div>
@@ -23,11 +23,11 @@ export default {
             required: true,
         },
     },
-    filters: {
-        formatData(value) {
-            return new Date(value);
-        },
-    },
+    // filters: {
+    //     formatData(value) {
+    //         return new Date(value);
+    //     },
+    // },
     methods: {
         async deleteItem() {
             try {

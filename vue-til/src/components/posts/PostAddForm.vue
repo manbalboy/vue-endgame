7a76707cc37569364a74255d6943fd70a94b1290
manbalboy <manbalboy@hanmail.net>
2021-02-23 18:16:34 +0900

    contents 길이 validation 적용

--- vue-til/src/components/posts/PostAddForm.vue
+++ vue-til/src/components/posts/PostAddForm.vue
@@ -15,6 +15,9 @@
                         rows="5"
                         v-model="contents"
                     />
+                    <p v-if="!isContentsValid" class="validation-text warning">
+                        Contents must be less than 200
+                    </p>
                 </div>
                 <button type="submit" class="btn">Create</button>
             </form>
@@ -34,6 +37,11 @@ export default {
             contents: '',
             logMessage: '',
         };
+    },
+    computed: {
+        isContentsValid() {
+            return this.contents.length < 200;
+        },
     },
     methods: {
         async submitForm() {

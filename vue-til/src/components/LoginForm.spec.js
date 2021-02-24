a655ad69b13dcbb7b72272db9cac4c078bb7cea8
manbalboy <manbalboy@hanmail.net>
2021-02-24 09:39:10 +0900

    테스트코드 실습 find html 요소검색

---
+++ vue-til/src/components/LoginForm.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils';
+import LoginForm from './LoginForm.vue';
+
+describe('LoginForm.vue', () => {
+    test('ID 는 이메일 형식이어야 한다.', () => {
+        const wrapper = shallowMount(LoginForm);
+
+        //find 는 템플릿 안에 있는 html 요소를 찾아갈수있음
+        const usernameInput = wrapper.find('#username');
+        console.log('usernameInput >>>>> ', usernameInput.html());
+        console.log(usernameInput);
+    });
+});

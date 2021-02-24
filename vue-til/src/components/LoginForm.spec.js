582d30e31e0c15801d814098502d21bcdc1e28ff
manbalboy <manbalboy@hanmail.net>
2021-02-24 09:44:16 +0900

    element value 접근

--- vue-til/src/components/LoginForm.spec.js
+++ vue-til/src/components/LoginForm.spec.js
@@ -3,11 +3,19 @@ import LoginForm from './LoginForm.vue';
 
 describe('LoginForm.vue', () => {
     test('ID 는 이메일 형식이어야 한다.', () => {
-        const wrapper = shallowMount(LoginForm);
+        const wrapper = shallowMount(LoginForm, {
+            data() {
+                return {
+                    username: 'data 속성',
+                };
+            },
+        });
 
         //find 는 템플릿 안에 있는 html 요소를 찾아갈수있음
         const usernameInput = wrapper.find('#username');
         console.log('usernameInput >>>>> ', usernameInput.html());
+        console.log('usernameInput >>>>> ', usernameInput.element.value);
+        console.log('usernameInput >>>>> ', usernameInput.element);
         console.log(usernameInput);
     });
 });
